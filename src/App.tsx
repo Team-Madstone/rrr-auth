131e0e2ff3a8308e00376edb38af3ef0112823c3
hanbin-son <50011531+madstone-dev@users.noreply.github.com>
2023-02-06 01:01:42 +0900

    :sparkles: add Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "src/pages/Home";
+import Login from "src/pages/Login";
+import SignUp from "src/pages/SignUp";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
